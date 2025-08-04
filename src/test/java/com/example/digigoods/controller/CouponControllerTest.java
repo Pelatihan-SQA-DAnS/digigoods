0a5a7336af00eaf51f38672b0d8694dbed6a36b0
Daya Adianto <addianto@users.noreply.github.com>
2025-08-04 17:38:16 +0700

    chore: Replace deprecated MockBean annotation with MockitoBean

--- src/test/java/com/example/digigoods/controller/CouponControllerTest.java
+++ src/test/java/com/example/digigoods/controller/CouponControllerTest.java
@@ -20,8 +20,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(CouponController.class)
@@ -33,7 +33,7 @@ class CouponControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
-  @MockBean
+  @MockitoBean
   private CouponService couponService;
 
   @Test
--- src/test/java/com/example/digigoods/controller/DiscountControllerTest.java
+++ src/test/java/com/example/digigoods/controller/DiscountControllerTest.java
@@ -19,8 +19,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(DiscountController.class)
@@ -32,7 +32,7 @@ class DiscountControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
-  @MockBean
+  @MockitoBean
   private DiscountCalculationService discountCalculationService;
 
   @Test
